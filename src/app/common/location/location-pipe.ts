50c435e19c00c5f59ed4286f5ce8f763baac2aa8
vamsi.krishna <krishnavamsi165@gmail.com>
2025-07-03 15:00:09 +0530

    Step: 15

---
+++ src/app/common/location/location-pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ILocation } from '../../workshops/models/Iworkshop';
+
+@Pipe({
+  name: 'location'
+})
+export class LocationPipe implements PipeTransform {
+
+ transform(value: ILocation, format: 'short'|'long' = 'long'): string {
+    if(format === 'short') {
+      return `${value.address}`;
+    } else {
+      return `${value.address}`
+    }
+    return "";
+  }
+
+}
