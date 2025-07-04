85786ed549a7f07880bf5121bb7c634e57e6bcef
vamsi.krishna <krishnavamsi165@gmail.com>
2025-07-04 22:04:11 +0530

    Step 27: Using environment files for enabling environment-based settings

--- src/app/workshops/sessions.ts
+++ src/app/workshops/sessions.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import ISession from './models/ISession';
+import { environment } from '../../environments/environment';
 
 export type VoteType = 'upvote' | 'downvote';
 
@@ -8,18 +9,19 @@ export type VoteType = 'upvote' | 'downvote';
   providedIn: 'root'
 })
 export class Sessions {
+  apiUrl = environment.apiUrl;
 
   constructor( private http : HttpClient) { }
 
   getSessionsFromWorkshop(workshopId:number) {
     return this.http.get<ISession[]>(
-      `https://workshops-server.onrender.com/workshops/${workshopId}/sessions`
+      `${this.apiUrl}/workshops/${workshopId}/sessions`
     );
   }
 
   voteForSession(sessionId: number, voteType: VoteType) {
     return this.http.put<ISession>(
-      `https://workshops-server.onrender.com/sessions/${sessionId}/${voteType}`,
+      `${this.apiUrl}/sessions/${sessionId}/${voteType}`,
       null  //we genrally pass data in put request. In this case we don't have any data
     );
   }
