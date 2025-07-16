d5b2d4c8f11a7621f609fa5b9aa9dc8b889fe70e
vamsi.krishna <krishnavamsi165@gmail.com>
2025-07-16 16:19:43 +0530

    removing files from repo

--- src/app/common/location/location-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LocationPipe } from './location-pipe';
-
-describe('LocationPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LocationPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
